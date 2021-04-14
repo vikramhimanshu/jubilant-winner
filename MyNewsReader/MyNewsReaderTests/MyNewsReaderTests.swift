a0ac0131b469025086c67a8c0e5408ad2d8f9db8
Himanshu Vikram <himanshu@kreativapps.com>
2021-04-14 23:05:59 +0530

    Default Project

---
+++ MyNewsReader/MyNewsReaderTests/MyNewsReaderTests.swift
@@ -0,0 +1,33 @@
+//
+//  MyNewsReaderTests.swift
+//  MyNewsReaderTests
+//
+//  Created by Himanshu T on 14/4/21.
+//
+
+import XCTest
+@testable import MyNewsReader
+
+class MyNewsReaderTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
